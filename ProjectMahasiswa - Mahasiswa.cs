84eb83c2409e2ac20ba5c133040ea05e8cdc20b8
Muhammad Rizal Saputro <79897743+Rizal2003@users.noreply.github.com>
2021-07-13 16:30:30 +0700

    Create ProjectMahasiswa - Mahasiswa.cs

---
+++ ProjectMahasiswa - Mahasiswa.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string Kelamin { get; set; }
+        public string Ipk { get; set; }
+
+    }
+}
